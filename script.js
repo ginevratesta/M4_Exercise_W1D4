596c37992ef7b7795dfd2d64c8be13fb3186e41b
Ginevra Testa <ginevra99testaa@gmail.com>
2023-12-22 17:03:59 +0100

    modal

--- script.js
+++ script.js
@@ -7,19 +7,28 @@ fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem')
 .catch(error => {
     console.error('Error fetching data:', error);
   });
-    
+
 let song = document.querySelector("#card-container")
+let tracks = document.querySelector(".modal-body")
     function setAlbumCards(cards){
     console.log('Songs',cards)
     for(const card of cards){
         song.innerHTML += `
-        <div class = "col-3"
+        <div class = "col-6 col-sm-6 col-md-4 col-lg-3"
         <div class="card">
         <img src="${card.album.cover_medium}" class="card-img-top" alt="...">
-        <div class="card-body d-flex justify-content-center py-2">
+        <div class="card-body d-flex align-items-center flex-column py-2">
           <p class="card-text">${card.album.title}</p>
         </div>
       </div>
       </div>`
+
+      tracks.innerHTML += `<div class="d-flex align-items-center">
+      <div>
+      <img src="${card.album.cover_small}" class="card-img-top" alt="...">
+      </div>
+      <p>${card.album.title}</p>
+      </div>
+      `
     }
  }
